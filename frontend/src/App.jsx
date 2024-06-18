1b8e317e4801d3d1aa740420cf7fc73ca77a2383
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2024-06-18 17:12:24 -0400

    frontend/feat/setup and installations

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <div className='text-5xl text-center text-blue-600 font-bold'>
+      <h1>Hello World</h1>
+   </div>
+  )
+}
+
+export default App
