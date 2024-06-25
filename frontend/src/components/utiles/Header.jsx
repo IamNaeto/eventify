8c7927d9b69012f1f848aa6dd9d2fc404d2c8600
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2024-06-25 18:30:53 -0400

    frontend/feat/implement logout and profile header tab

--- frontend/src/components/utiles/Header.jsx
+++ frontend/src/components/utiles/Header.jsx
@@ -1,37 +1,105 @@
 import { Link } from "react-router-dom";
 import Auths from "../modal/Auths";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { HiOutlineBell } from "react-icons/hi2";
+import { BiLogOutCircle } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
+import { Toaster, toast } from "sonner";
+import { TbLoader2 } from "react-icons/tb";
 
 const Header = () => {
-    const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    const openModal = () => setModalIsOpen(true);
-    const closeModal = () => setModalIsOpen(false);
+  const navigate = useNavigate();
 
-    return ( 
-        <div className="w-full fixed z-30 bg-[#FEFEFE] flex items-center justify-between py-4 px-[3%] shadow text-[16px] text-[#3C3C3C] font-semibold">
-            <Link to={"/"}>
-                <img src="/img/eventify-logo.svg" alt="" />
-            </Link>
+  useEffect(() => {
+    const token = localStorage.getItem("eventify_auth_token");
+    if (token) {
+      setIsAuthenticated(true);
+    }
+    setIsLoading(false);
+  }, []);
 
-            <div className="flex items-center justify-center gap-6">
-                <Link to={""} className="hover:text-[#E0580C] delay-150 transition-all">Explore</Link>
-                <Link to={"/manage/events"} className="hover:text-[#E0580C] delay-150 transition-all">Manage Events</Link>
-                <Link to={"/event/create"} className="hover:text-[#E0580C] delay-150 transition-all">Create Event +</Link>
-            </div>
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
 
-            <div className="flex items-center justify-center gap-6">
-                <button 
-                className="border-2 border-[#E0580C] bg-[#FEFEFE] hover:text-[#E0580C] hover:shadow-lg transition-all delay-150 py-2 px-4 rounded-md"
-                onClick={openModal}
-                >Sign in</button>
-                <Link to={"/event/create"} className="border-2 border-[#E0580C] hover:border-[#9D3E08] bg-[#E0580C] hover:bg-[#9D3E08] transition-all delay-150 text-[#FEFEFE] py-2 px-4 rounded-md">Create Event</Link>
-            </div>
+  const handleLogOut = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      localStorage.removeItem("eventify_auth_token");
+      setIsAuthenticated(false);
+      navigate("/");
+      toast.success("Logout successful");
+      setIsLoading(false);
+    }, 3000);
+  };
 
-             <Auths closeModal={closeModal} modalIsOpen={modalIsOpen}/>
-            
+  return (
+    <div className="w-full fixed z-30 bg-[#FEFEFE] flex items-center justify-between py-4 px-[3%] shadow text-[16px] text-[#3C3C3C] font-semibold">
+      <Link to={"/"}>
+        <img src="/img/eventify-logo.svg" alt="" />
+      </Link>
+
+      <div className="flex items-center justify-center gap-6">
+        <Link to={""} className="hover:text-[#E0580C] delay-150 transition-all">
+          Explore
+        </Link>
+        <Link
+          to={"/manage/events"}
+          className="hover:text-[#E0580C] delay-150 transition-all"
+        >
+          Manage Events
+        </Link>
+        <Link
+          to={"/event/create"}
+          className="hover:text-[#E0580C] delay-150 transition-all"
+        >
+          Create Event +
+        </Link>
+      </div>
+      {!isAuthenticated ? (
+        <div className="flex items-center justify-center gap-6">
+          <button
+            className="border-2 border-[#E0580C] bg-[#FEFEFE] hover:text-[#E0580C] hover:shadow-lg transition-all delay-150 py-2 px-4 rounded-md"
+            onClick={openModal}
+          >
+            Sign in
+          </button>
+          <Link
+            to={"/event/create"}
+            className="border-2 border-[#E0580C] hover:border-[#9D3E08] bg-[#E0580C] hover:bg-[#9D3E08] transition-all delay-150 text-[#FEFEFE] py-2 px-4 rounded-md"
+          >
+            Create Event
+          </Link>
         </div>
-     );
-}
- 
+      ) : (
+        <div className="flex items-center justify-center gap-4 text-[#E0580C] hover:text-[#9D3E08] delay-100 transition-all">
+          <div className="text-3xl shadow-lg p-1 rounded-full  border border-[#EBEBEB] cursor-pointer">
+            <HiOutlineBell />
+          </div>
+
+          <div className="p-2 rounded-full shadow-lg border border-[#EBEBEB] cursor-pointer">
+            <img src="/img/event-user.png" alt="user" />
+          </div>
+
+          <div
+            onClick={handleLogOut}
+            className="text-3xl shadow-lg p-1 rounded-full  border border-[#EBEBEB] cursor-pointer"
+          >
+            {isLoading ? (
+              <TbLoader2 className="animate-spin" />
+            ) : (
+              <BiLogOutCircle />
+            )}
+          </div>
+        </div>
+      )}
+
+      <Auths closeModal={closeModal} modalIsOpen={modalIsOpen} />
+    </div>
+  );
+};
+
 export default Header;
